d0711397db1806ee5706d7b834a3c77b8510f1b5
gillsonpassos <oliveirapassos07@outlook.com>
2023-08-10 12:41:34 -0300

    Herança

--- scripts.js
+++ scripts.js
@@ -201,3 +201,30 @@ console.log(shirt.productWithDiscount(50));
 const tenis = new Product("tenis verde", 120);
 
 console.log(tenis.productWithDiscount(20));
+
+// 9 herança
+class ProductWithAttributes extends Product {
+  constructor(name, price, colors) {
+    super(name, price);
+    this.colors = colors;
+  }
+
+  showColors() {
+    console.log("As cores são:");
+    this.colors.forEach((color) => {
+      console.log(color);
+    });
+  }
+}
+
+const hat = new ProductWithAttributes("Chapeu", 29.99, [
+  "Preto",
+  "Azul",
+  "Verde",
+]);
+
+console.log(hat.name);
+
+console.log(hat.productWithDiscount(30));
+
+hat.showColors();
