3b6562eb4205afd0a08e79ad49f40851f17c905f
gillsonpassos <oliveirapassos07@outlook.com>
2023-09-22 12:00:52 -0300

    Criando header e footer

---
+++ 16_todo_react/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// components
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+     <div>
+      <Header />
+  <h1>Conteudo...</h1>
+  <Footer />
+ </div>
+ );
+  
+}
+
+export default App;
