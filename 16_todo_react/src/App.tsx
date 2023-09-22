a7c45720f8dc0f064c9eaad2c70eef613c6d8a8d
gillsonpassos <oliveirapassos07@outlook.com>
2023-09-22 16:33:13 -0300

    iniciando ações do formulário

--- 16_todo_react/src/App.tsx
+++ 16_todo_react/src/App.tsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // components
 import Header from './components/Header';
 import Footer from './components/Footer';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
 
 // css
 import styles from "./App.module.css"
 
+// Interfaces
+import { ITask } from './interfaces/Task';
+
 function App() {
+
+ const [taskList, setTaskList] = useState<ITask[]>([]);
+
   return (
      <div>
       <Header />
   <main className={styles.main}>
-    <h1>Conteudo...</h1>
+    <div>
+      <h2>O que você vai faser?</h2>
+      <TaskForm btnText='Criar Tarefa' taskList={taskList} setTaskList={setTaskList}/>
+    </div>
+    <div>
+      <h2>Suas tarefas</h2>
+      <TaskList />
+    </div>
   </main>
   <Footer />
  </div>
